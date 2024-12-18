0efda148ec70a0866faa05c360587f96cf7afc80
Felicia <feliciamihaela2@gmail.com>
2024-12-18 20:51:06 +0200

    the game now has a rudimentary interface

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+let gameboard = document.querySelector('.gameboard');
+
 function Gameboard(){
     let board = [];
-
+    let cells =[];
     for(let i =0;i<9;i++){
+        let cell=document.createElement('div');
+        cell.classList.add("cell");
         board.push('E');
+        gameboard.appendChild(cell);
+        cell.dataset.index = i;
+        cell.textContent = board[i];
+        cells.push(cell);
     }
+    
 
     const getBoard=() => board;
 
@@ -18,9 +27,10 @@ function Gameboard(){
 
     const resetBoard=()=>{
         board = ['E','E','E','E','E','E','E','E','E'];
+        cells.forEach(cell=>cell.innerHTML='E');
     };
 
-    return {getBoard, addMark,resetBoard};
+    return {getBoard, addMark,resetBoard, cells};
 }
 
 function Player(name, mark){
@@ -58,7 +68,6 @@ function Game(){
         }
     }
 
-    
     let winningCombinations =[
         [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[0,4,8],[3,5,4],[6,7,8],[2,4,6]
     ]
@@ -70,51 +79,38 @@ function Game(){
     };
        
 
-    const playRound=(index)=>{
-        let result = setMark(index,currentPlayer);
-       if(result==='Invalid move'){
-        return result;
-       }
+    const playRound=()=>{
+        board.cells.forEach(cell=>cell.addEventListener('click',()=>{
+            const index = parseInt(cell.dataset.index); 
+            let result = setMark(index,currentPlayer);
+            if(result==='Invalid move'){
+                console.log(result) ;
+            }
+            if(result!=='Invalid move'){
+                cell.textContent = currentPlayer.mark;
+            }
+            if(checkWin(currentPlayer.playerChoices)){
+                
+                alert( `Player ${currentPlayer.name} won!`);
+                newGame();
+            }
+            if(endTie(board)===true){
+                
+                alert( 'Game Over');
+                newGame();
+            }
 
-       if(checkWin(currentPlayer.playerChoices)){
-        newGame();
-        return `Player ${currentPlayer.name} won!`;
-       }
-       if(endTie(board)===true){
-        newGame();
-        return 'Game Over';
-       }
-
-       currentPlayer = currentPlayer === Player1 ? Player2 : Player1;
-        return result;
+            currentPlayer = currentPlayer === Player1 ? Player2 : Player1;
+            return result;
+            })
+        )
     }
     return{playRound}
 }
 
 const game = Game();
-
-//Testing
-//p1 wins
-console.log(game.playRound(1));//p1
-console.log(game.playRound(3));//p2
-console.log(game.playRound(2));
-console.log(game.playRound(8));
-console.log(game.playRound(0));
-console.log(game.playRound(8));
+game.playRound();
 
 
-
-
-//toata lumea pierde
-// console.log(game.playRound(0)); // P1
-// console.log(game.playRound(1)); // P2
-// console.log(game.playRound(2)); // P1
-// console.log(game.playRound(4)); // P2
-// console.log(game.playRound(3)); // P1
-// console.log(game.playRound(5)); // P2
-// console.log(game.playRound(7)); // P1
-// console.log(game.playRound(6)); // P2
-// console.log(game.playRound(8)); // P1 (last move)
-// console.log(game.playRound(1));
 
 
